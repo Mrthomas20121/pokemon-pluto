cc4fcb1f5c49146684586a4708dd1328258e0adb
Mrthomas20121 <thomas77300@gmail.com>
2021-07-08 10:00:51 +0200

    Moved Pokemon Data to PokemonData

--- src/main/java/mrthomas20121/pokemon_pluto/api/pokemon/Pokemon.java
+++ src/main/java/mrthomas20121/pokemon_pluto/api/pokemon/Pokemon.java
@@ -9,33 +9,14 @@ public class Pokemon {
     private final PokemonType first_type;
     private final PokemonType second_type;
 
-    private int starting_level;
-    private int level;
-
-    private Object helditem;
-
-    public Pokemon(String name, int starting_level, PokemonType type1, PokemonType type2) {
+    public Pokemon(String name, PokemonType type1, PokemonType type2) {
         this.name = name;
-        this.level = starting_level;
-        this.starting_level = starting_level;
         this.first_type = type1;
         this.second_type = type2;
     }
 
     public String getName() {
         return name;
-    }
-
-    public void setHeldItem(Object helditem) {
-        this.helditem = helditem;
-    }
-
-    public void removeHeldItem() {
-        this.helditem = null;
-    }
-
-    public Object getHeldItem() {
-        return helditem;
     }
 
     public PokemonType getFirstType() {
@@ -48,17 +29,5 @@ public class Pokemon {
 
     public boolean hasTwoTypes() {
         return this.second_type != null;
-    }
-
-    public void levelup() {
-        this.level++;
-    }
-
-    public int getStartingLevel() {
-        return starting_level;
-    }
-
-    public int getLevel() {
-        return level;
     }
 }
--- src/main/java/mrthomas20121/pokemon_pluto/api/pokemon/PokemonData.java
+++ src/main/java/mrthomas20121/pokemon_pluto/api/pokemon/PokemonData.java
@@ -1,9 +1,28 @@
 package mrthomas20121.pokemon_pluto.api.pokemon;
 
+import mrthomas20121.pokemon_pluto.api.item.Item;
+
+/**
+ * PokemonData.java -
+ * @author Mrthomas20121
+ * This class contain the data of a pokemon.
+ * 
+ *
+ * pokemon_name - This is the pokemon name.
+ * starting_level - This is the level you found the pokemon at.
+ * level - This is the current pokemon level.
+ * totalxp - This is the amount of xp the pokemon have.
+ * ev - The pokemon ev.
+ * iv - The pokemon iv.
+ * xpGain - Whether or not the pokemon should receive xp.
+ * xpBonuses - This is the XP Bonuses From outside source(Item and other stuff, e.g multi exp)
+ * heldItem - The Item Held by the pokemon
+ */
 public class PokemonData {
 
     private String pokemon_name;
 
+    private int starting_level;
     private int level;
     private double totalxp;
 
@@ -14,10 +33,10 @@ public class PokemonData {
 
     private int[] xpBonuses;
 
-    private Object heldItem;
+    private Item heldItem;
 
     public Pokemon getPokemon() {
-        return null;// Manager.getPokemon();
+        return null;// Manager.getPokemonByName();
     }
 
     public void setXPGain(boolean xpGain) {
